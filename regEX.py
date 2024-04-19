4d3dd0f4594a585c44a999ef6c8261d4acc56ce6
Anil Gadekar <40288220+anilgadekar2805@users.noreply.github.com>
2024-04-19 14:05:34 +0200

    Update regEX.py
    Test

--- regEX.py
+++ regEX.py
@@ -11,3 +11,20 @@ pattern = re.compile(r'\.\d{17}(?=\D|$)')
 trimmed_files = [pattern.sub('', file) for file in files]
 
 print(trimmed_files)
+
+
+
+import re
+
+files = [
+    'Anil.Gadekar.22.20240419000000',
+    'Raj.Gade.22.20240419000000.NOK',
+    'Alan.marks.DDPP.21202404'
+]
+
+pattern = re.compile(r'(\.\d{17}|\.NOK)(?=\D|$)')
+
+trimmed_files = [pattern.sub('', file) for file in files]
+
+print(trimmed_files)
+
